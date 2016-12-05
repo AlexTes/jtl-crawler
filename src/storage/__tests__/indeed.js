18d99c96e4e50b52bcb400b610ef735f1256329e
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2016-12-05 02:22:54 +0100

    Add first job storage

---
+++ src/storage/__tests__/indeed.js
@@ -0,0 +1,26 @@
+/* @flow */
+import { omit } from 'lodash';
+import rdbdBuilder from 'rethinkdbdash';
+import * as job from '../indeed';
+import mockJob from '../__mocks__/indeed-job.json';
+
+const r = rdbdBuilder({
+  db:     'jtl',
+  silent: true,
+});
+
+function stripGeneratedProps(indeedJob: IndeedJob): RawIndeedJob {
+  return omit(indeedJob, 'createdAt', 'id', 'updatedAt');
+}
+
+it('should store an indeed job successfully', async () => {
+  const id = await job.store(mockJob);
+  const rawJob = await r.table('indeed').get(id);
+  const cleanJob = stripGeneratedProps(rawJob);
+  expect(cleanJob).toEqual(mockJob);
+});
+
+afterAll(async () => {
+  await r.table('indeed').filter({ source: 'Mock' }).delete().run();
+  await r.getPoolMaster().drain();
+});
---
+++ src/storage/indeed.js
@@ -0,0 +1,75 @@
+/* @flow */
+import rdbdBuilder from 'rethinkdbdash';
+
+const r = rdbdBuilder({
+  db:     'jtl',
+  silent: true,
+});
+
+export type RawIndeedJob = {
+  jobtitle: string,
+  company: string,
+  city: string,
+  state: string,
+  country: string,
+  formattedLocation: string,
+  source: string,
+  date: string,
+  snippet: string,
+  url: string,
+  onmousedown: string,
+  latitude: number,
+  longitude: number,
+  jobkey: string,
+  sponsored: boolean,
+  expired: boolean,
+  indeedApply: boolean,
+  formattedLocationFull: string,
+  formattedRelativeTime: string,
+  stations: string,
+};
+
+export type IndeedJob = {
+  jobtitle: string,
+  company: string,
+  city: string,
+  state: string,
+  country: string,
+  formattedLocation: string,
+  source: string,
+  date: string,
+  snippet: string,
+  url: string,
+  onmousedown: string,
+  latitude: number,
+  longitude: number,
+  jobkey: string,
+  sponsored: boolean,
+  expired: boolean,
+  indeedApply: boolean,
+  formattedLocationFull: string,
+  formattedRelativeTime: string,
+  stations: string,
+  createdAt: string,
+  id: string,
+  updatedAt: string,
+}
+
+export function store(job: RawIndeedJob): Promise<string> {
+  return r
+    .table('indeed')
+    .insert({
+      createdAt: r.now(),
+      updatedAt: r.now(),
+      ...job,
+    })
+    .run()
+    .then(result => result.generated_keys[0]);
+}
+
+export function retrieve(id: string): Promise<IndeedJob> {
+  return r
+    .table('indeed')
+    .get(id)
+    .run();
+}
